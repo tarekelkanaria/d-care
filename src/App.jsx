5cf231bac45c4bf50359fdfe02dcfb95997faecd
tarekelkanaria <tarekelkanaria@gmail.com>
2026-05-18 09:20:50 +0300

    Modify login form

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./components/Layout/Layout";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./components/NotFound/NotFound";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 
 const routes = createBrowserRouter([
   {
